89fd17aa1c0a3a3255be6feea31bf7ae31110fa8
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-10-04 15:37:19 +0545

    created some sections and components and designed hero section

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Nav, Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer } from "./sections"
+const App = () => {
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero/>
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services/>
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className=" bg-pale-blue padding">
+        <CustomerReviews/>
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe/>
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer/>
+      </section>
+    </main>
+  )
+}
+
+export default App
