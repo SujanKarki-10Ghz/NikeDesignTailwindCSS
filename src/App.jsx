c75bb924f0f56be26987fdfb69e44b4d40b4cd1b
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-10-05 10:28:36 +0545

    completed hero section, popular products section and superquality section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { Nav, Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer } from "./sections"
+import {
+  Nav,
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
 const App = () => {
   return (
     <main className="relative">
       <Nav />
-      <section className="xl:padding-1 wide:padding-r padding-b">
-        <Hero/>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
       </section>
       <section className="padding">
         <PopularProducts />
@@ -13,22 +23,22 @@ const App = () => {
         <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        <Services/>
+        <Services />
       </section>
       <section className="padding">
         <SpecialOffers />
       </section>
       <section className=" bg-pale-blue padding">
-        <CustomerReviews/>
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe/>
+        <Subscribe />
       </section>
       <section className=" bg-black padding-x padding-t pb-8">
-        <Footer/>
+        <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
